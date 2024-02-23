506a5f4dd5202f36237515875cefff1bd7ad6c1a
Joan Perez Alvarado <joanjpx>
2024-02-22 20:47:50 -0500

    Fix Eureka Clients

--- microservice-product/src/main/java/com/microservice/product/ProductController.java
+++ microservice-product/src/main/java/com/microservice/product/ProductController.java
@@ -1,6 +1,19 @@
 package main.java.com.microservice.product;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class ProductController {
     
+    @GetMapping("/json")
+    public String json() {
+        
+        return "{\"message\":\"hello\"}";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
--- microservice-shipping/src/main/java/com/microservice/shipping/ShippingController.java
+++ microservice-shipping/src/main/java/com/microservice/shipping/ShippingController.java
@@ -1,6 +1,19 @@
 package main.java.com.microservice.shipping;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class ShippingController {
     
+    @GetMapping("/json")
+    public String json() {
+        
+        return "{\"message\":\"hello\"}";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
