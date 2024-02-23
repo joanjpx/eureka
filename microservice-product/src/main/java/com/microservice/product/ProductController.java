9e359fa7f65fae2cbe581c5e6fc32e22ad6f0cb1
Joan Perez Alvarado <joanjpx>
2024-02-22 20:21:09 -0500

    Fixes in Eureka Server

---
+++ microservice-product/src/main/java/com/microservice/product/ProductController.java
@@ -0,0 +1,6 @@
+package main.java.com.microservice.product;
+
+@RestController
+public class ProductController {
+    
+}
---
+++ microservice-shipping/src/main/java/com/microservice/shipping/ShippingController.java
@@ -0,0 +1,6 @@
+package main.java.com.microservice.shipping;
+
+@RestController
+public class ShippingController {
+    
+}
